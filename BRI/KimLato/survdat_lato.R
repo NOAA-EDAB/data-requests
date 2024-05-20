aece6161f2f15acabe5b418edbf27c71f8336ff8
andybeet <22455149+andybeet@users.noreply.github.com>
2024-05-20 17:26:35 -0400

    BRI shape file and resulting rds file

--- BRI/KimLato/survdat_lato.R
+++ BRI/KimLato/survdat_lato.R
@@ -14,12 +14,75 @@ channel <- dbutils::connect_to_database("NEFSC_USERS","abeet")
 ## pull survey data
 data <- survdat::get_survdat_data(channel)
 
+# select unique lat and longs for plotting
+points <- data$survdat |>
+  dplyr::select("LAT","LON") |>
+  dplyr::distinct()
+
+
 # specify spatial footprint
-area1 <- sf::st_read(dsn = here::here("BRI/KimLato/gis/"),quiet=T)
-area2 <- sf::st_read(dsn = here::here("BRI/KimLato/gis/"),quiet=T)
+area1 <- sf::st_read(dsn = here::here("BRI/KimLato/gis/Empire_60km_rounded.shp"),quiet=T)
+area2 <- sf::st_read(dsn = here::here("BRI/KimLato/gis/MassRI_60km_rounded.shp"),quiet=T)
 
-# calculate swept area biomass
-calc_swept_area(surveyData=data$survdat, areaPolygon=area, areaDescription="EPU", filterByArea="all",filterBySeason = "all",tidy=T)
+# plot stations on polygon map
+survdat::plot_data_area(points=points, polygons=area1)
+
+# calculate swept area biomass by season for each area
+# spring area1
+spr1 <- survdat::calc_swept_area(surveyData=data$survdat,
+                areaPolygon=area1,
+                areaDescription="Id",
+                filterByArea="all",
+                filterBySeason = "SPRING",
+                tidy=T) |>
+  dplyr::mutate(SEASON = "SPRING",
+                AREA = "Empire")
+# fall area1
+fall1 <- survdat::calc_swept_area(surveyData=data$survdat,
+                areaPolygon=area1,
+                areaDescription="Id",
+                filterByArea="all",
+                filterBySeason = "FALL",
+                tidy=T)   |>
+  dplyr::mutate(SEASON = "FALL",
+                AREA = "Empire")
+
+# spring area2
+spr2 <- survdat::calc_swept_area(surveyData=data$survdat,
+                                 areaPolygon=area2,
+                                 areaDescription="Shape_Leng",
+                                 filterByArea="all",
+                                 filterBySeason = "SPRING",
+                                 tidy=T)   |>
+  dplyr::mutate(SEASON = "SPRING",
+                AREA = "MassRI")
+
+# fall area2
+fall2 <- survdat::calc_swept_area(surveyData=data$survdat,
+                                  areaPolygon=area2,
+                                  areaDescription="Shape_Leng",
+                                  filterByArea="all",
+                                  filterBySeason = "FALL",
+                                  tidy=T)   |>
+  dplyr::mutate(SEASON = "FALL",
+                AREA = "MassRI")
 
 
-##
+# concatenate all and select total biomass variable + variance
+d <- rbind(spr1,spr2,fall1,fall2) |>
+  dplyr::filter(variable %in% c("tot.biomass","tot.bio.var"))
+
+# pull species from survey and join by SVSPP
+species <- survdat::get_species(channel)$data |>
+  dplyr::select(SVSPP,COMNAME,SCINAME) |>
+  dplyr::filter(!is.na(SVSPP)) |>
+  dplyr::distinct()
+
+finalData <- d |>
+  dplyr::left_join(species,by = "SVSPP") |>
+  dplyr::as_tibble() |>
+  dplyr::filter(!(SVSPP == 122))
+
+#save output aas an rds file
+saveRDS(finalData,here::here("BRI/KimLato/dataRequest.rds"))
+
