8d566d206eadc768eea524879e1617c67ef78708
andybeet <22455149+andybeet@users.noreply.github.com>
2024-06-06 12:14:05 -0400

    changed Atlantic Mackerel to Atlanti mackerel (lowe case m) - added old survdta function in hope of recreating past

--- EwE-menhaden-AndreBuchheister/2014request/Andre_data_pulls_v2.R
+++ EwE-menhaden-AndreBuchheister/2014request/Andre_data_pulls_v2.R
@@ -41,7 +41,7 @@ strata <- readOGR(gis.dir, 'strata')
 regions <- c('mab.shallow', 'mab.mid', 'mab.deep', 'sne.shallow', 'sne.mid', 'sne.deep',
                             'gb.mid',  'gb.deep',  'gom.shallow', 'gom.mid', 'gom.deep')
 
-mab.shallow <- c(03150:03440) 
+mab.shallow <- c(03150:03440)
 mab.mid     <- c(01610, 01650, 01690, 01730)
 mab.deep    <- c(01620:01640, 01660:01680, 01700:01720, 01740:01760)
 sne.shallow <- c(03010:03140, 03450:03560)
@@ -49,7 +49,7 @@ sne.mid     <- c(01010, 01050, 01090)
 sne.deep    <- c(01020:01040, 01060:01080, 01100:01120)
 gb.mid      <- c(01190, 01200, 01250)
 gb.deep     <- c(01130:01180, 01210:01230)
-gom.shallow <- c(03570:03590, 03620:03640, 03670, 03680, 03700, 03710, 03730, 
+gom.shallow <- c(03570:03590, 03620:03640, 03670, 03680, 03700, 03710, 03730,
                  03740, 03760, 03790, 03820, 03840, 03860, 03880)
 gom.mid     <- c(03600, 03610, 03650, 03660, 03690, 03720, 03750, 03770, 03780,
                  03800, 03810, 03830, 03850, 03870, 03890)
@@ -87,7 +87,7 @@ andre <- andre[!is.na(Region), ]
 #Set up for stratification
 #Break size structured groups
 andre[SVSPP %in% c(15, 36, 69, 72, 73, 76:78, 103, 105, 135, 139, 145), MULTI := T]
-andre[is.na(MULTI), MULTI := F] 
+andre[is.na(MULTI), MULTI := F]
 
 single.box <- andre[MULTI == F, ]
 
@@ -134,7 +134,7 @@ setkey(multi.box, CRUISE6, STRATUM, STATION, SVSPP, SIZE)
 multi.box[, box.biomass := sum(WGTLEN), by = key(multi.box)]
 
 #Alter SVSPP so prestrat treats as seperate species
-multi.box[SIZE == 'SMALL',  SVSPP := SVSPP + 1000L] 
+multi.box[SIZE == 'SMALL',  SVSPP := SVSPP + 1000L]
 multi.box[SIZE == 'MEDIUM', SVSPP := SVSPP + 2000L]
 multi.box[SIZE == 'LARGE',  SVSPP := SVSPP + 3000L]
 ewe.spp[EWE %like% '\\(S)', SVSPP := SVSPP + 1000L]
@@ -158,18 +158,18 @@ for(i in 1:length(regions)){
   #Segregate by region
   andre.region <- andre[Region == regions[i], ]
   andre.region[, Region := NULL] #Will get added back in next step
-  
+
   #Run pre stratmean function
   andre.pre <- prestrat(andre.region, stratum, strat.col = 'STRATUM', area.col = 'STRATUM_AREA')
-  
+
   #Reduce number of species
   pre.ewe.box <- merge(andre.pre, ewe.spp, by = 'SVSPP')
-  
+
   #Calculate stratified mean
   ewe.box <- stratmean(pre.ewe.box, group.col = 'SVSPP', strat.col = 'STRATUM')
 
   #Calculate swept area
-  ewe.biomass <- sweptarea(pre.ewe.box, ewe.box, q = ewe.spp[, list(SVSPP, fallq)], 
+  ewe.biomass <- sweptarea(pre.ewe.box, ewe.box, q = ewe.spp[, list(SVSPP, fallq)],
                            strat.col = 'STRATUM', area.col = 'STRATUM_AREA', group.col = 'SVSPP')
 
   #Collapse species to EwE boxes
@@ -181,7 +181,7 @@ for(i in 1:length(regions)){
   ewe.biomass[, c('Tot.biomass', 'Tot.abundance', 'SVSPP') := NULL]
   setnames(ewe.biomass, c('sum.biomass', 'sum.abund'), c('Tot.biomass', 'Tot.abundance'))
   ewe.biomass[, Region := regions[i]]
-  setcolorder(ewe.biomass, c('Region',        'EWE',        'YEAR', 
+  setcolorder(ewe.biomass, c('Region',        'EWE',        'YEAR',
                              'strat.biomass', 'biomass.S2', 'biomass.SE', 'Tot.biomass',
                              'strat.abund',   'abund.S2',   'abund.SE',   'Tot.abundance'))
 
