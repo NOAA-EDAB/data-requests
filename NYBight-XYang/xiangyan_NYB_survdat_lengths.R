bd710fafdbe5f56da2f2780408e7ec25bf9b352e
andybeet <22455149+andybeet@users.noreply.github.com>
2025-08-06 10:16:10 -0400

    added lengths for strata

--- NYBight-XYang/xiangyan_NYB_survdat_lengths.R
+++ NYBight-XYang/xiangyan_NYB_survdat_lengths.R
@@ -17,12 +17,22 @@
 
 channel <- dbutils::connect_to_database(server = server, uid = user)
 
+## pul survey data with lengths
 data_length <- survdat::get_survdat_data(channel = channel, getLengths = T)
+
 data <- data_length
 
+# strata of interest
 NYBstrata <- c(3010:3200, 1010:1080, 1730:1760)
 
+data_length$survdat <- data_length$survdat |>
+  dplyr::filter(STRATUM %in% NYBstrata)
+data_length$notes <- "The full survdat pull was filterd by STRATA = c(3010:3200, 1010:1080, 1730:1760)"
 
+saveRDS(data_length, here::here("NYBight-XYang/NYBstrata_lengths.rds"))
+
+
+# swept area by season in strata of interest
 NYBspring <- survdat::calc_swept_area(
   surveyData = data$survdat,
   filterByArea = NYBstrata,
@@ -35,7 +45,7 @@ NYBfall <- survdat::calc_swept_area(
   filterBySeason = "FALL"
 )
 
-
+# stratified mean by season in strata of interest
 NYBspring_mean <- survdat::calc_stratified_mean(
   surveyData = data$survdat,
   filterByArea = NYBstrata,
@@ -48,7 +58,7 @@ NYBfall_mean <- survdat::calc_stratified_mean(
   filterBySeason = "FALL"
 )
 
-
+# save all output
 saveRDS(NYBspring, here::here("NYBight-XYang/NYBspring_lengths.rds"))
 saveRDS(NYBfall, here::here("NYBight-XYang/NYBfall_lengths.rds"))
 saveRDS(
@@ -61,5 +71,6 @@ saveRDS(NYBfall_mean, here::here("NYBight-XYang/NYBfall_mean_lengths.rds"))
 area <- sf::st_read(
   dsn = system.file("extdata", "strata.shp", package = "survdat")
 )
-areas <- survdat::get_area(areaPolygon = area, areaDescription = "STRATA")
+areas <- survdat::get_area(areaPolygon = area, areaDescription = "STRATA") |>
+  dplyr::filter(STRATUM %in% NYBstrata)
 saveRDS(areas, here::here("NYBight-XYang/NYBstrata_areas.rds"))
