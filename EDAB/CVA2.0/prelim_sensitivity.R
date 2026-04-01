aed1b552e90c95d9a277a793bb06d8f3b82cf80f
Scott Large <1456859158113302@mil>
2026-04-01 12:54:33 -0400

    make names anon

--- EDAB/CVA2.0/prelim_sensitivity.R
+++ EDAB/CVA2.0/prelim_sensitivity.R
@@ -1,12 +1,19 @@
 # remotes::install_github("NEFSC/READ-EDAB-CVA2.0")
 source("https://raw.githubusercontent.com/NEFSC/READ-EDAB-CVA2.0/refs/heads/main/R/make_sensitivity_barplots.R")
+library(dplyr)
+library(stringi)
 
-dat <- read.csv(here::here("EDAB/CVA2.0", "export_stocks_csv__.csv"))
+dat <- read.csv(here::here("EDAB/CVA2.0", "export_stocks_csv__.csv")) %>% 
+  group_by(Scorer) %>% 
+  mutate(Scorer = stri_rand_strings(length = 5, n = 1, pattern = "[A-Z]"))
+  
 
-data = dat
-species = unique(dat$Stock.Name)
-plots_name = here::here("EDAB/CVA2.0/figures/prelim_sensitivity")
-unique(dat$Stock.Name)
+
+
+#  data = dat
+# species = unique(dat$Stock.Name)
+# plots_name = here::here("EDAB/CVA2.0/figures/prelim_sensitivity")
+# unique(dat$Stock.Name)
 
 
 make_sensitivity_barplots(data = dat, species = unique(dat$Stock.Name),
